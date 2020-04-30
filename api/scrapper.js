47629a75e9d3532e392f2306fccf93e4ba531b46
swssr <simocmo4@gmail.com>
2020-04-30 15:42:32 +0200

    Clean up

--- api/scrapper.js
+++ api/scrapper.js
@@ -2,15 +2,20 @@ const Scraper = require("images-scraper");
 
 const google = new Scraper({
   puppeteer: {
-    headless: false,
+    headless: true,
   },
 });
 
 module.exports = async (req, res) => {
   try {
-    const results = await google.scrape("6007652000529", 10);
-    res.status(200).json(results);
+    if (req.params.query) {
+      const results = await google.scrape(req.params.query, 4);
+
+      res.status(200).json({ results });
+    } else {
+      throw new Error("No query provided!");
+    }
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ error });
   }
 };
